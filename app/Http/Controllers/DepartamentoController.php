ff41f32ceff021b2f32059e0063c501fd00ab89c
patricio <patricioprp06@gmail.com>
2021-08-21 23:30:39 -0300

    previo a deploy

--- app/Http/Controllers/DepartamentoController.php
+++ app/Http/Controllers/DepartamentoController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Departamento;
 
 use Illuminate\Http\Request;
 
 class DepartamentoController extends Controller
 {
     public function all(){
-        
+        return response()->json(['Msg' => Departamento::with('comunas.escuelas','seccion')->get()],200);
     }
 }
